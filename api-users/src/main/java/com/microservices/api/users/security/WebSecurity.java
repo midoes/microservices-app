e61155aa9d6a28027d2d93ae6523c4de2165da5d
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-05-28 12:32:35 -0500

    Added a custom login URL

--- api-users/src/main/java/com/microservices/api/users/security/WebSecurity.java
+++ api-users/src/main/java/com/microservices/api/users/security/WebSecurity.java
@@ -35,6 +35,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	private AuthenticationFilter getAuthenticationFilter() throws Exception {
 		AuthenticationFilter authenticationFilter = new AuthenticationFilter(userService, env, authenticationManager());
 		// authenticationFilter.setAuthenticationManager(authenticationManager());
+		authenticationFilter.setFilterProcessesUrl(env.getProperty("login.url.path"));
 		return authenticationFilter;		
 	}
 	
