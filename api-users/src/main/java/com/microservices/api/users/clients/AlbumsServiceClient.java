ee869ac9d2fa6a8b93b84add2199b0480084225d
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-06-03 10:00:30 -0500

    Added Hystrix to ws-users

--- api-users/src/main/java/com/microservices/api/users/clients/AlbumsServiceClient.java
+++ api-users/src/main/java/com/microservices/api/users/clients/AlbumsServiceClient.java
@@ -1,18 +1,30 @@
 package com.microservices.api.users.clients;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservices.api.users.dto.AlbumDTO;
 
-@FeignClient(name="albums-ws")
+@FeignClient(name="albums-ws", fallback=AlbumsFallback.class)
 public interface AlbumsServiceClient {
 	
 	// Forcing 404 error Bad Request
-	@GetMapping("/users/{id}/albumss")
+	@GetMapping("/users/{id}/albums")
 	public List<AlbumDTO> getAlbums(@PathVariable String id);
  
+}
+
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+
+	@Override
+	public List<AlbumDTO> getAlbums(String id) {		 		
+		return new ArrayList<AlbumDTO>();
+	}
+
 }
