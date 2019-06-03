555e116407fd715d992b37c13dfb883eb2b6b028
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-06-03 10:19:29 -0500

    Added error handling with feign hystrix factory

--- api-users/src/main/java/com/microservices/api/users/clients/AlbumsServiceClient.java
+++ api-users/src/main/java/com/microservices/api/users/clients/AlbumsServiceClient.java
@@ -3,6 +3,8 @@ package com.microservices.api.users.clients;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservices.api.users.dto.AlbumDTO;
 
-@FeignClient(name="albums-ws", fallback=AlbumsFallback.class)
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+
+@FeignClient(name="albums-ws", fallbackFactory=AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
 	
 	// Forcing 404 error Bad Request
@@ -20,11 +25,37 @@ public interface AlbumsServiceClient {
 }
 
 @Component
-class AlbumsFallback implements AlbumsServiceClient {
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
 
 	@Override
-	public List<AlbumDTO> getAlbums(String id) {		 		
+	public AlbumsServiceClient create(Throwable cause) {
+		return new AlbumsServiceClientFallback(cause);
+	}
+	
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	private final Throwable cause;
+	
+	public AlbumsServiceClientFallback(Throwable cause) {
+		this.cause = cause;
+	}
+	
+	@Override
+	public List<AlbumDTO> getAlbums(String id) {
+		
+		if(cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+			logger.error("404 error took place when getAlbums was called with id: "
+					+ id + "Error message: "
+					+ cause.getLocalizedMessage());
+		} else {
+			logger.error("Other error took place: " + cause.getLocalizedMessage());
+		}
+		
 		return new ArrayList<AlbumDTO>();
 	}
-
+	
 }
